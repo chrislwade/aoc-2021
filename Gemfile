5eae4d6e70ba26d7feb66f9b6b5b5ca736e571c5
Christopher Wade <wade.christopher@gmail.com>
2021-12-10 00:48:31 -0600

    Add day 10.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'dotenv'
+gem 'http'
 gem 'pry'
 gem 'rspec'
 gem 'rspec-file_fixtures'
---
+++ lib/day10.rb
@@ -0,0 +1,66 @@
+require 'day'
+
+class Day10 < Day
+  def self.expected
+    {puzzle1: 26397, puzzle2: 288957}
+  end
+
+  def display
+    puts "--- Day 10: Syntax Scoring ---"
+    puts "What is the total syntax error score for those errors?"
+    puts "- Puzzle 1: #{puzzle1}"
+    puts "What is the middle score?"
+    puts "- Puzzle 2: #{puzzle2}"
+  end
+
+  def puzzle1
+    lines.map {|line| corruption_score(line)}.compact.sum
+  end
+
+  def puzzle2
+    lines.reject {|line| corruption_score(line) > 0}.map {|line| completion_score(line)}.sort.middle
+  end
+
+  protected
+
+  def corruption_score(line)
+    stack = []
+
+    line.chars.each do |char|
+      if OPEN.keys.include?(char)
+        stack.push(char)
+      elsif stack.last != CLOSE[char]
+        return CORRUPTION_POINTS[char]
+      else
+        stack.pop
+      end
+    end
+
+    return 0
+  end
+
+  def completion_score(line)
+    stack = []
+
+    line.chars.each do |char|
+      if OPEN.keys.include?(char)
+        stack.push(char)
+      else
+        stack.pop
+      end
+    end
+
+    stack.reverse.map {|char| OPEN[char]}.reduce(0) {|acc, char| acc * 5 + COMPLETION_POINTS[char]}
+  end
+
+  CORRUPTION_POINTS = { ')' => 3, ']' => 57, '}' => 1197, '>' => 25137 }
+  COMPLETION_POINTS = { ')' => 1, ']' => 2, '}' => 3, '>' => 4 }
+  OPEN = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+  CLOSE = OPEN.invert
+end
+
+class Array
+  def middle
+    self[length / 2]
+  end
+end
